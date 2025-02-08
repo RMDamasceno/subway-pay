87a1bc95013678c4be2346d9c0097d6c5c4d76b3
Rafael Damasceno <user.name like "Rafael Damasceno>
2025-02-08 17:56:06 -0300

    modified:   App/conectarbanco.php Utilizar variaveis do ambiente Docker.

--- App/conectarbanco.php
+++ App/conectarbanco.php
@@ -1,10 +1,10 @@
 <?php
 
 $config = array(
-    'db_host' => 'localhost',
-    'db_user' => 'u295945790_subaposta',
-    'db_pass' => 'jsQYU@=*Tf[7',
-    'db_name' => 'u295945790_subaposta'
+    'db_host' => getenv('DB_HOST') ?: 'subwaypay-db',
+    'db_user' => getenv('DB_USER') ?: 'subwaypay',
+    'db_pass' => getenv('DB_PASS') ?: 'subwaypay',
+    'db_name' => getenv('DB_NAME') ?: 'subwaypay'
 );
 
 ?>
